ee4c0753c4a70a155092c4c0585b814fdc06fd71
Tony Fuentes <leobender@gmail.com>
2016-08-29 16:15:54 -0700

    added function to save to local storage, and populate transactions

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,8 @@
-// Categories //
-// -Rent
-// -Food
-// -Utils
-// -Entertainment
-// -Clothes
-// -Car/Auto/Gas
-// -Vacation
-// -Bills
-// -Gifts
-// -Medical
-
-
-
-// Create New User Account
-
-
-function USER(state){
-    this.state = state;
-    this.userProfile;
-    this.transactions = [];
-}
+var PROGSTATE = {
+  accountNum: 500,
+  Users: [],
+  StateName: "fiscState"
+};
 
 function User (userName, firstName, lastName, accountNumber, email, address) {
     this.userName = userName;
@@ -36,152 +19,67 @@ function Address (street, city, state, zip) {
     this.state = state;
     this.zip = zip;
 }
-function Transaction (id, type, date, location, amount, tag) {
+function Transaction (id, date, description, amount, tag) {
     this.transactionID = id;
     this.date = date;
-    this.loc = location;
+    this.description = description;
     this.amount = amount;
     this.tag = tag;
 }
 
-function Tag = (){
-}
-var users = [];
+function populateTransactions(jsonArray, currentUser) {
+  var transactionID;
+  for(var i=0; i<jsonArray.length; ++i){
+    var transaction = new Transaction (jsonArray[i].transactionID, jsonArray[i].date, jsonArray[i].description, jsonArray[i].amount, jsonArray[i].tag)
+    currentUser.transactions.push(transaction);
+  }
 
-USER.prototype.addTransaction = function(date, location, amount, tag){
-    var newTransaction = new Transaction(date, location, amount, tag);
-    USER.
-}
-
-
-
-
-// user.transactions[user.transactions.length] = new Transaction('12/25/16', 'Fred Meyer', '$35.17');
-//
-// var anotherTrans =new Transaction('12/25/10', 'Meijer', '$35.34');
-
-
-// Search Accounts On Login
-// for (var i = 0; i < array.length; i++) {
-//   if (newUser[i].userName === 'dafunk'){
-//     // success!!!!!
-//     // return newUser[i];
-//   }
-// }
-
-
-
-// Back End Logix
-// function BankAccount(name, password, accountNumber){
-//   this.user = name;
-//   this.password = password;
-//   this.balance = 20;
-//   this.accountNumber = accountNumber;
-// }
-// var accounts = [];
-
-// var userWithdrawl = 5;
-// BankAccount.prototype.withdrawl = function () {
-//   return this.balance - userWithdrawl;
-// }
-// var userDeposit = 5;
-// BankAccount.prototype.deposit = function(){
-//   return this.balance + userDeposit;
-// }
-
-function leftPad(number, targetLength) {
-    var output = number + '';
-    while (output.length < targetLength) {
-        output = '0' + output;
-    }
-    return output;
-}
-// Confirm unique username
-function findDuplicate(newUserName) {
-    for (var i = 0; i < users.length; i++) {
-        if (users[i].userName === newUserName) {
-            return true;
-        }
-        return false;
-    }
 }
 
-// Verify user and password
-function verifyUser(username, password) {
-    var userTest = localStorage.getItem(username)
-    if (userTest === null) {
-        return alert('Username not found');
-    } else {
-        userTest = JSON.parse(userTest);
-        if (userTest[0].password === password) {
-            return userTest;
-        } else {
-            alert("Please enter the correct Password")
-        }
-        console.log(userTest);
-    }
+// Send User to localStorage, with accountNumber as identifier
+function storeUser(User){
+  localStorage.setItem(User.accountNumber, JSON.stringify(User));
 }
-var accountCounter = 989087;
-// JSON.parse(retrievedObject));
-// Front end Logic
+
+// Retrieve User from localStorage, with accountNumber as identifier
+function retrieveUser(LSNAME) {
+  var lsArray = JSON.parse(localStorage.getItem(LSNAME));
+  var address = new Address (lsArray.address.street, lsArray.address.city, lsArray.address.state, lsArray.address.zip);
+  var retrievedUser = new User (lsArray.userName, lsArray.firstName, lsArray.lastName, lsArray.accountNumber, lsArray.email, address);
+  for(var i=0; i<lsArray.transactions.length; ++i){
+    var transaction = new Transaction;
+    transaction.transactionID = lsArray.transactions[i].transactionID;
+    transaction.date = lsArray.transactions[i].date;
+    transaction.description = lsArray.transactions[i].description;
+    transaction.amount = lsArray.transactions[i].amount;
+    transaction.tag = lsArray.transactions[i].tag;
+    retrievedUser.transactions.push(transaction);
+  }
+  return retrievedUser;
+}
+
+function createNewUser(userName, firstName, lastName, email, street, city, state, zip, jsonData) {
+  var accountNumber = PROGSTATE.accountNum++;
+  var newUser = new User (userName, firstName, lastName, accountNumber, email, new Address (street, city, state, zip));
+
+  if(jsonData != null){
+    populateTransactions(jsonData, newUser);
+  }
+
+  PROGSTATE.Users.push(newUser);
+  return newUser;
+}
+
+
 $(document).ready(function(){
-    $('#goToLogin').click(function() {
-        $('#accountCreator').hide();
-        $('#accountLogin').show();
-    });
-    $('#goToCreate').click(function() {
-        $('#accountLogin').hide();
-        $('#accountCreator').show();
-    });
-    $('#home').click(function() {
-        $('#accountLogin').hide();
-        $('#accountCreator').hide();
-    });
-    $('#account-creator').submit(function(event){
-        event.preventDefault();
-        var newUserName = $('input#new-username').val();
-        var newPassword = $('input#new-password').val();
-        var newFirstName = $('input#new-first').val();
-        var newLastName = $('input#new-last').val();
-        var newEmail = $('input#new-email').val();
-        var newStreet = $('input#new-street').val();
-        var newCity = $('input#new-city').val();
-        var newState = $('input#new-state').val();
-        var newZip = $('input#new-zip').val();
-        var duplicate = findDuplicate(newUserName);
-        if (duplicate) {
-            $('input#new-password').val("");
-            $('input#new-username').val("");
-            alert("Username unavailable");
-        } else {
-            localStorage.setItem('accountCounter', accountCounter+=11);
-            var accountNumber = localStorage.getItem('accountCounter');
-            var newAddress = new Address(newStreet, newCity, newState, newZip);
-            var user = new User(newUserName, newFirstName, newLastName, accountNumber, newEmail, newAddress);
-            users.push(user);
-            $('input#new-password').val("");
-            $('input#new-username').val("");
-            $('input#new-first').val("");
-            $('input#new-last').val("");
-            $('input#new-email').val("");
-            $('input#new-street').val("");
-            $('input#new-city').val("");
-            $('input#new-state').val("");
-            $('input#new-zip').val("");
-        }
-    });
-    // $('#accountLogin').submit(function(event){
-    //   event.preventDefault();
-    //   var username = $('input#username').val();
-    //   var password = $('input#password').val();
-    //   currentUser = verifyUser(username, password);
-    //   $("#balance").append("<p class='displayAccount'>" + currentUser[0].user + ", Current Balance: " + currentUser[0].balance + "</p>");;
-    //   $('input#username').val("");
-    //   $('input#password').val("");
-    // });
-    // $('#add').click(function() {
-    //   currentUser[0][2] + 5;
-    //   currentUser[0].deposit();
-    //   localStorage.setItem(currentUser[1],currentUser);
-    // });
+
+  var ricky = createNewUser("ricky", "Rick", "James", "rickjames@bitch.com","1010 Main St", "Portland", "OR", "97214", rickyJsonData);
+  var bobby = createNewUser("bobby", "Rick", "James", "rickjames@bitch.com","1010 Main St", "Portland", "OR", "97214", rickyJsonData);
+  storeUser(ricky);
+
+  debugger;
+
+
+
+
 });
