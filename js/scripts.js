764a3afe0853a2436f3029af52cdba918d05f31a
Jason Downing <jhdowning@gmail.com>
2016-08-30 13:23:41 -0700

    add login function to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -156,6 +156,23 @@ function refreshUserList () {
 
 //var accountCounter = 989086;
 
+// Login Function
+function revealLogin (evt, type) {
+
+  var tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  // Get all elements with class="tablinks" and remove the class "active"
+  var tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(type).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
 // Front end Logic
 $(document).ready(function(){
 
