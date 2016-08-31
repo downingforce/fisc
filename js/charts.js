a94f10584c0d9ae795571c8184858d0ef276a1a5
Ben Vissotzky <ben.vissotzky@gmail>
2016-08-31 10:35:26 -0700

    merged charts onto one page, corrected table update

--- js/charts.js
+++ js/charts.js
@@ -17,15 +17,18 @@ var series = [{
  name: 'Automotive',
  data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 }, {
- name: 'Bills',
+ name: 'Other',
  data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 }, {
  name: 'Medical',
  data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+}, {
+ name: 'Home-Improvement',
+ data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 }];
 
 function sortLineChart(){
-  var thisUser = PROGSTATE.Users[PROGSTATE.CurrentUser];
+  var thisUser = PROGSTATE.Users[0];
 
   for (var i = 0; i < thisUser.transactions.length; i++) {
     var date = new Date(thisUser.transactions[i].date).getMonth();
@@ -47,18 +50,31 @@ function sortLineChart(){
     if (thisUser.transactions[i].tag === 'automotive') {
       series[5].data[date] += parseFloat(thisUser.transactions[i].amount);
     }
-    if (thisUser.transactions[i].tag === 'bills') {
+    if (thisUser.transactions[i].tag === 'other') {
       series[6].data[date] += parseFloat(thisUser.transactions[i].amount);
     }
     if (thisUser.transactions[i].tag === 'medical') {
       series[7].data[date] += parseFloat(thisUser.transactions[i].amount);
     }
+    if (thisUser.transactions[i].tag === 'home-improvement') {
+      series[8].data[date] += parseFloat(thisUser.transactions[i].amount);
+    }
   }
 }
 $(function () {
 
+  $('#toggle-line').click(function() {
+    $('#pie-chart').hide();
+    $('#line-chart').show();
+    });
+
+  $('#toggle-pie').click(function() {
+    $('#pie-chart').show();
+    $('#line-chart').hide();
+    });
 
 
+sortLineChart();
   // Line Chart
   $('#line-chart').highcharts({
     title: {
@@ -97,104 +113,111 @@ $(function () {
 
   });
   // End Line Chart
-
+  //
   // Pie Chart
-  
-  // var thisUser = PROGSTATE.Users[PROGSTATE.CurrentUser];
-  // var rent = 0.0;
-  // var food = 0.0;
-  // var utilities = 0.0;
-  // var entertainment = 0.0;
-  // var clothes = 0.0;
-  // var automotive = 0.0;
-  // var bills = 0.0;
-  // var medical = 0.0;
   //
-  // for (var i = 0; i < thisUser.transactions.length; i++) {
-  //
-  //     if(thisUser.transactions[i].tag === 'Rent'){
-  //         rent += parseFloat(thisUser.transactions[i].amount);
-  //     }
-  //     if(thisUser.transactions[i].tag === 'Food'){
-  //         food += parseFloat(thisUser.transactions[i].amount);
-  //     }
-  //     if(thisUser.transactions[i].tag === 'Utilities'){
-  //         utilities += parseFloat(thisUser.transactions[i].amount);
-  //     }
-  //     if(thisUser.transactions[i].tag === 'Entertainment'){
-  //         entertainment += parseFloat(thisUser.transactions[i].amount);
-  //     }
-  //     if(thisUser.transactions[i].tag === 'Clothes'){
-  //         clothes += parseFloat(thisUser.transactions[i].amount);
-  //     }
-  //     if(thisUser.transactions[i].tag === 'Automotive'){
-  //         automotive += parseFloat(thisUser.transactions[i].amount);
-  //     }
-  //     if(thisUser.transactions[i].tag === 'Bills'){
-  //         Bills += parseFloat(thisUser.transactions[i].amount);
-  //     }
-  //     if(thisUser.transactions[i].tag === 'Medical'){
-  //         medical += parseFloat(thisUser.transactions[i].amount);
-  //     }
-  // }
+  var thisUser = PROGSTATE.Users[0];
+  var rent = 0.0;
+  var food = 0.0;
+  var utilities = 0.0;
+  var entertainment = 0.0;
+  var clothes = 0.0;
+  var automotive = 0.0;
+  var medical = 0.0;
+  var other = 0.0;
+  var homeImprovement = 0.0;
+
+  for (var i = 0; i < thisUser.transactions.length; i++) {
+
+      if(thisUser.transactions[i].tag === 'rent'){
+          rent += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'food'){
+          food += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'utilities'){
+          utilities += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'entertainment'){
+          entertainment += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'clothes'){
+          clothes += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'automotive'){
+          automotive += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'medical'){
+          medical += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'home-improvement'){
+          homeImprovement += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'other'){
+          other += parseFloat(thisUser.transactions[i].amount);
+      }
+  }
 
 
-  // $('#pie-chart').highcharts({
-  //   chart: {
-  //     plotBackgroundColor: null,
-  //     plotBorderWidth: null,
-  //     plotShadow: false,
-  //     type: 'pie'
-  //   },
-  //   title: {
-  //     text: 'Annual Spending: Grouped by tags'
-  //   },
-  //   tooltip: {
-  //     pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-  //   },
-  //   plotOptions: {
-  //     pie: {
-  //       allowPointSelect: true,
-  //       cursor: 'pointer',
-  //       dataLabels: {
-  //         enabled: true,
-  //         format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-  //         style: {
-  //           color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-  //         }
-  //       }
-  //     }
-  //   },
-  //   series: [{
-  //     name: 'Brands',
-  //     colorByPoint: true,
-  //     data: [{
-  //       name: 'Rent',
-  //       y: rent
-  //     }, {
-  //       name: 'Food',
-  //       y: food
-  //     }, {
-  //       name: 'Utilities',
-  //       y: utilities
-  //     }, {
-  //       name: 'Entertainment',
-  //       y: entertainment
-  //     }, {
-  //       name: 'Clothes',
-  //       y: clothes
-  //     }, {
-  //       name: 'Automotive',
-  //       y: automotive
-  //     },{
-  //       name: 'Bills',
-  //       y: bills
-  //     },{
-  //       name: 'Medical',
-  //       y: medical
-  //     }]
-  //   }]
-  // });
+  $('#pie-chart').highcharts({
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    title: {
+      text: 'Annual Spending: Grouped by tags'
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+          style: {
+            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+          }
+        }
+      }
+    },
+    series: [{
+      name: 'Brands',
+      colorByPoint: true,
+      data: [{
+        name: 'Rent',
+        y: rent
+      }, {
+        name: 'Food',
+        y: food
+      }, {
+        name: 'Utilities',
+        y: utilities
+      }, {
+        name: 'Entertainment',
+        y: entertainment
+      }, {
+        name: 'Clothes',
+        y: clothes
+      }, {
+        name: 'Automotive',
+        y: automotive
+      },{
+        name: 'Other',
+        y: other
+      },{
+        name: 'Medical',
+        y: medical
+      }, {
+        name: 'Home-Improvement',
+        y: homeImprovement
+      }]
+    }]
+  });
   // End Pie Chart
 
 });
--- js/tables.js
+++ js/tables.js
@@ -1,3 +1,4 @@
+
 function findTrans(id) {
   var thisUser = PROGSTATE.Users[PROGSTATE.CurrentUser];
   for (var i = 0; i <= thisUser.transactions.length; i++) {
@@ -7,16 +8,19 @@ function findTrans(id) {
   }
 }
 
-
+// Converts Transactions object into array for table
 var transactionData = [];
-for (var i = 0; i < rickyJsonData.length; i++) {
-  array = [rickyJsonData[i].transactionID, rickyJsonData[i].date, rickyJsonData[i].description,rickyJsonData[i].amount,rickyJsonData[i].tag]
+for (var i = 0; i < PROGSTATE.Users[PROGSTATE.CurrentUser].transactions.length; i++) {
+  array = [PROGSTATE.Users[PROGSTATE.CurrentUser].transactions[i].transactionID, PROGSTATE.Users[PROGSTATE.CurrentUser].transactions[i].date, PROGSTATE.Users[PROGSTATE.CurrentUser].transactions[i].description,PROGSTATE.Users[PROGSTATE.CurrentUser].transactions[i].amount,PROGSTATE.Users[PROGSTATE.CurrentUser].transactions[i].tag]
   transactionData.push(array);
 }
 $(document).ready(function() {
 
 
+
+  // sets iniital value for transactionID on page, starts with new unused transactionID
   document.getElementById("trans-id").value = transactionData.length + 1;
+  // Creates DataTable
   var transactions = $('#transaction-list').DataTable( {
       data: transactionData,
       columns: [
@@ -28,20 +32,21 @@ $(document).ready(function() {
       ]
   });
 
-
+// Provides id of row selected and passes it's transaction data into input fields
   $('#transaction-list tbody').on('click', 'tr', function () {
+
           var data = transactions.row( this ).data();
           $('#add-trans').hide();
           $('#update-trans').show();
           document.getElementById("trans-id").value = data[0];
-          // var tempObject = findTrans(id);
-          // $('#trans-date').val(tempObject.date)
-          // $('#trans-desc').val(tempObject.desc)
-          // $('#trans-amt').val(tempObject.amount)
-          // $('#trans-tag').val(tempObject.tag)
+          var tempObject = findTrans(data[0]);
+          document.getElementById("trans-date").value = tempObject.date
+          document.getElementById("trans-desc").value = tempObject.description
+          document.getElementById("trans-amt").value = tempObject.amount
+          document.getElementById("trans-tag").value = tempObject.tag
       });
 
-
+// Pushes new transactions into both object array of transactions and into datatable array
   $('#add-trans').on( 'click', function () {
     var transID = document.getElementById("trans-id").value;
     var transDate = document.getElementById("trans-date").value;
@@ -54,4 +59,33 @@ $(document).ready(function() {
     PROGSTATE.Users[PROGSTATE.CurrentUser].transactions.push(transID,transDate,transDesc,transAmt,transTag)
     document.getElementById("trans-id").value = transactionData.length + 1
   });
+  // when user clicks, takes input and corrects specific transaction
+  $('#update-trans').click(function(event) {
+    var transID = document.getElementById("trans-id").value;
+    var transDate = document.getElementById("trans-date").value;
+    var transDesc = document.getElementById("trans-desc").value;
+    var transAmt = document.getElementById("trans-amt").value;
+    var transTag = document.getElementById("trans-tag").value;
+
+    PROGSTATE.Users[PROGSTATE.CurrentUser].transactions[transID-1].date = transDate;
+    PROGSTATE.Users[PROGSTATE.CurrentUser].transactions[transID-1].desc = transDesc;
+    PROGSTATE.Users[PROGSTATE.CurrentUser].transactions[transID-1].amount = transAmt;
+    PROGSTATE.Users[PROGSTATE.CurrentUser].transactions[transID-1].tag = transTag;
+    transactionData[transID - 1] = [transID,transDate,transDesc,transAmt,transTag];
+
+    $('#add-trans').show();
+    $('#update-trans').hide();
+
+    $('#trans-date').val('')
+    $('#trans-desc').val('')
+    $('#trans-amt').val('')
+    $('#trans-tag').val('')
+
+    transactions.clear().draw();
+    transactions.rows.add(transactionData); // Add new data
+    transactions.columns.adjust().draw(); // Redraw the DataTable
+
+    event.preventDefault();
+  });
+  // end of transaction update
 });
