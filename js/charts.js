b42edf2e61ad7aa8e7c58dc77cf285a235853cb0
Ben Vissotzky <ben.vissotzky@gmail>
2016-08-31 16:29:58 -0700

    fix chart function commit

--- js/charts.js
+++ js/charts.js
@@ -1,77 +1,33 @@
-function setPieChart(){
-  var thisUser = PROGSTATE.Users[PROGSTATE.CurrentUser];
-  var rent = 0.0;
-  var food = 0.0;
-  var utilities = 0.0;
-  var entertainment = 0.0;
-  var clothes = 0.0;
-  var automotive = 0.0;
-  var medical = 0.0;
-  var other = 0.0;
-  var homeImprovement = 0.0;
+var series = [{
+ name: 'Rent',
+ data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+}, {
+ name: 'Food',
+ data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+}, {
+ name: 'Utilities',
+ data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+}, {
+ name: 'Entertainment',
+ data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+}, {
+ name: 'Clothes',
+ data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+}, {
+ name: 'Automotive',
+ data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+}, {
+ name: 'Other',
+ data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+}, {
+ name: 'Medical',
+ data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+}, {
+ name: 'Home-Improvement',
+ data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+}];
 
-  for (var i = 0; i < thisUser.transactions.length; i++) {
-
-    if(thisUser.transactions[i].tag === 'rent'){
-      rent += parseFloat(thisUser.transactions[i].amount);
-    }
-    if(thisUser.transactions[i].tag === 'food'){
-      food += parseFloat(thisUser.transactions[i].amount);
-    }
-    if(thisUser.transactions[i].tag === 'utilities'){
-      utilities += parseFloat(thisUser.transactions[i].amount);
-    }
-    if(thisUser.transactions[i].tag === 'entertainment'){
-      entertainment += parseFloat(thisUser.transactions[i].amount);
-    }
-    if(thisUser.transactions[i].tag === 'clothes'){
-      clothes += parseFloat(thisUser.transactions[i].amount);
-    }
-    if(thisUser.transactions[i].tag === 'automotive'){
-      automotive += parseFloat(thisUser.transactions[i].amount);
-    }
-    if(thisUser.transactions[i].tag === 'medical'){
-      medical += parseFloat(thisUser.transactions[i].amount);
-    }
-    if(thisUser.transactions[i].tag === 'home-improvement'){
-      homeImprovement += parseFloat(thisUser.transactions[i].amount);
-    }
-    if(thisUser.transactions[i].tag === 'other'){
-      other += parseFloat(thisUser.transactions[i].amount);
-    }
-  }
-}
-
-
-function setLineGraph(){
-  var series = [{
-    name: 'Rent',
-    data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-  }, {
-    name: 'Food',
-    data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-  }, {
-    name: 'Utilities',
-    data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-  }, {
-    name: 'Entertainment',
-    data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-  }, {
-    name: 'Clothes',
-    data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-  }, {
-    name: 'Automotive',
-    data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-  }, {
-    name: 'Other',
-    data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-  }, {
-    name: 'Medical',
-    data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-  }, {
-    name: 'Home-Improvement',
-    data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-  }];
+function sortLineChart(){
   var thisUser = PROGSTATE.Users[0];
 
   for (var i = 0; i < thisUser.transactions.length; i++) {
@@ -117,25 +73,22 @@ function findTotal() {
   }
   return totalsArray;
 }
-function setTotals(){
+
+function move(average) {
+    var elem = document.getElementById("budget-left");
+    elem.style.width = average + "%";
+
+}
+
+
+$(function () {
+
   var totals = findTotal()
   var incomeTotal = parseFloat(totals[0]);
   var expenseTotal = parseFloat(totals[1]);
   var savings = parseFloat(incomeTotal - expenseTotal);
-}
-function move(average) {
-  var elem = document.getElementById("budget-left");
-  elem.style.width = average + "%";
-
-}
-
-
-$(document).ready(function(){
-
-
 
   function populateBudget() {
-    setTotals();
     var income = '$' + incomeTotal.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
     var expense = '$' + expenseTotal.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
     var savingsString = '$' + savings.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
@@ -149,125 +102,169 @@ $(document).ready(function(){
     $('#pie-chart').hide();
     $('#line-chart').show();
     $('#budget-chart').hide();
-  });
+    });
 
   $('#toggle-pie').click(function() {
     $('#pie-chart').show();
     $('#line-chart').hide();
     $('#budget-chart').hide();
-  });
+    });
 
   $('#toggle-budget').click(function() {
     $('#pie-chart').hide();
     $('#line-chart').hide();
     $('#budget-chart').show();
-  });
-  // Line Graph
-
-  function printLineGraph(){
-    setLineGraph();
-    $('#line-chart').highcharts({
-      title: {
-        text: 'Monthly Spending Trends',
-        x: -20 //center
-      },
-      subtitle: {
-        text: 'Source: Your fisc account',
-        x: -20
-      },
-      xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-      },
-      yAxis: {
-        title: {
-          text: '$ Dollars $'
-        },
-        plotLines: [{
-          value: 0,
-          width: 1,
-          color: '#808080'
-        }]
-      },
-      tooltip: {
-        valuePrefix: '$'
-      },
-      legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle',
-        borderWidth: 0
-      },
-
-      series: series
-
     });
-  }// End Line Chart
+  populateBudget();
 
-  // Pie Chart
-  function printPieChart(){
-    setPieChart();
-    $('#pie-chart').highcharts({
-      chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
-      },
+
+
+
+
+sortLineChart();
+  // Line Chart
+  $('#line-chart').highcharts({
+    title: {
+      text: 'Monthly Spending Trends',
+      x: -20 //center
+    },
+    subtitle: {
+      text: 'Source: Your fisc account',
+      x: -20
+    },
+    xAxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+    yAxis: {
       title: {
-        text: 'Annual Spending: Grouped by tags'
+        text: '$ Dollars $'
       },
-      tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-      },
-      plotOptions: {
-        pie: {
-          allowPointSelect: true,
-          cursor: 'pointer',
-          dataLabels: {
-            enabled: true,
-            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-            style: {
-              color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-            }
+      plotLines: [{
+        value: 0,
+        width: 1,
+        color: '#808080'
+      }]
+    },
+    tooltip: {
+      valuePrefix: '$'
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      borderWidth: 0
+    },
+
+     series: series
+
+  });
+  // End Line Chart
+  //
+  // Pie Chart
+  //
+  var thisUser = PROGSTATE.Users[0];
+  var rent = 0.0;
+  var food = 0.0;
+  var utilities = 0.0;
+  var entertainment = 0.0;
+  var clothes = 0.0;
+  var automotive = 0.0;
+  var medical = 0.0;
+  var other = 0.0;
+  var homeImprovement = 0.0;
+
+  for (var i = 0; i < thisUser.transactions.length; i++) {
+
+      if(thisUser.transactions[i].tag === 'rent'){
+          rent += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'food'){
+          food += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'utilities'){
+          utilities += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'entertainment'){
+          entertainment += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'clothes'){
+          clothes += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'automotive'){
+          automotive += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'medical'){
+          medical += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'home-improvement'){
+          homeImprovement += parseFloat(thisUser.transactions[i].amount);
+      }
+      if(thisUser.transactions[i].tag === 'other'){
+          other += parseFloat(thisUser.transactions[i].amount);
+      }
+  }
+
+
+  $('#pie-chart').highcharts({
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    title: {
+      text: 'Annual Spending: Grouped by tags'
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+          style: {
+            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
           }
         }
-      },
-      series: [{
-        name: 'Allocation',
-        colorByPoint: true,
-        data: [{
-          name: 'Rent',
-          y: rent
-        }, {
-          name: 'Food',
-          y: food
-        }, {
-          name: 'Utilities',
-          y: utilities
-        }, {
-          name: 'Entertainment',
-          y: entertainment
-        }, {
-          name: 'Clothes',
-          y: clothes
-        }, {
-          name: 'Automotive',
-          y: automotive
-        },{
-          name: 'Other',
-          y: other
-        },{
-          name: 'Medical',
-          y: medical
-        }, {
-          name: 'Home-Improvement',
-          y: homeImprovement
-        }]
+      }
+    },
+    series: [{
+      name: 'Allocation',
+      colorByPoint: true,
+      data: [{
+        name: 'Rent',
+        y: rent
+      }, {
+        name: 'Food',
+        y: food
+      }, {
+        name: 'Utilities',
+        y: utilities
+      }, {
+        name: 'Entertainment',
+        y: entertainment
+      }, {
+        name: 'Clothes',
+        y: clothes
+      }, {
+        name: 'Automotive',
+        y: automotive
+      },{
+        name: 'Other',
+        y: other
+      },{
+        name: 'Medical',
+        y: medical
+      }, {
+        name: 'Home-Improvement',
+        y: homeImprovement
       }]
-    });
-  }// End Pie Chart
-  printPieChart();
-  printLineGraph();
-  populateBudget();
+    }]
+  });
+  // End Pie Chart
+
 });
